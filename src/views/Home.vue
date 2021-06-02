822b3a61e313e0b2a767ddbb6768a857d409a309
wuyuanwuhui99 <275018723@qq.com>
2021-06-03 00:06:27 +0800

    修改查询收藏频道问题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,35 @@
 <template>
     <div class="home">
         <ul id="nav-list">
-            <li class="nav-item" v-for="item,index in favoriteChannels" :key="'nav-item'+index">{{item.channelName}}</li>
+            <li class="nav-item" @click="tabChannel(item,index)" :class="{'nav-item-active':activeChannel == item}" v-for="item,index in favoriteChannels" :key="'nav-item'+index">{{item.channelName}}</li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
-    import {defineComponent,computed} from 'vue'
+    import {defineComponent,ref} from 'vue'
     import {getUserDataService,getFavoriteChannelsList} from "../service/homeService";
-    import { useStore } from 'vuex'
     import mapGetters from "../store/mapGetters";
+    import {ChannelsInterface} from "../types";
 
     export default defineComponent({
         name: 'Home',
         async setup() {
             await getUserDataService();//获取用户信息
             await getFavoriteChannelsList();//获取频道信息
+
             let {favoriteChannels} = mapGetters(["favoriteChannels"]);
+            let activeChannel:any  = ref(null)
+            activeChannel.value = favoriteChannels.value.find((item: ChannelsInterface) => item.status == 1)
+
+            const tabChannel = (item:ChannelsInterface)=>{
+                activeChannel.value = item
+            }
+
             return {
-                favoriteChannels
+                tabChannel,
+                activeChannel,
+                favoriteChannels,
             }
         }
     })
@@ -31,6 +41,7 @@
         display: flex;
         padding: 1rem;
         font-size: 1.2rem;
+        border-bottom: 1px solid #ddd;
         &::-webkit-scrollbar{
             display: none;
         }
